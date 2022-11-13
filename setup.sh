906e1790a75a78c3aa13c0eee7744d731c8912e4
Chitaso <>
2022-11-13 01:18:33 -0500

    change to drive link

--- setup.sh
+++ setup.sh
@@ -5,11 +5,11 @@ sudo apt install python3.6
 sudo apt install python3.6-distutils
 sudo apt-get install libsndfile1-dev
 
-# git clone https://github.com/Chitasa/HiFi-GAN.git
-# cd HiFi-GAN
 python3.6 -m pip install -r requirements.txt
 
-wget --content-disposition https://b.catgirlsare.sexy/ZLBcq5NYKaZu.zip
+#wget --content-disposition https://b.catgirlsare.sexy/ZLBcq5NYKaZu.zip
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1nCrOBI8my-yjFeGXMR2bbeaNvPHEIbgT' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1nCrOBI8my-yjFeGXMR2bbeaNvPHEIbgT" -O wavs.zip && rm -rf /tmp/cookies.txt
 unzip wavs.zip
+rm wavs.zip
 
 wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1oYS__mSNkUDb90s23CCw-Zy7XlPMGjyn' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1oYS__mSNkUDb90s23CCw-Zy7XlPMGjyn" -O zhongli_training && rm -rf /tmp/cookies.txt
